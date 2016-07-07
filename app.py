d38c5afa3c6cf5c5680a13867ee70f32ab6b545c
Watrous <mwatrous@ms.ds.uhc.com>
2016-07-07 12:50:26 -0400

    json fix

--- app.py
+++ app.py
@@ -70,13 +70,13 @@ def returnRXCUI(req):
 	drug = parameters.get("drug")
 	url = baseurl + "term=" + drug + "&maxEntries=1"
 	result = (requests.get(url)).json()
-	rxcui = result['0']
+	#rxcui = result['0']
 	#approximateGroup = result.get('approximateGroup')
 	#candidate = approximateGroup.get('candidate')
 	#rxcui = candidate.get('rxcui')
 	#makeWebhookResult???
 	#rxcui = "hello"
-	return rxcui;
+	return result;
 
 #def returnInteractions(rxcuiList):
 
