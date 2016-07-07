94ae65b57f67c3117dd61a9f3f71f97a3e6b168d
Watrous <mwatrous@ms.ds.uhc.com>
2016-07-07 16:00:58 -0400

    json fix

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ def returnInquiry(req):
 
 	result = result.text
 	lhs, rhs = result.split("INDICATIONS AND USAGE ",1)
-	lhs, rhs = rhs.split("\" ], \"set_id\":",1)
+	lhs, rhs = rhs.split("\"],",1)
 	inquiry = lhs
 	return inquiry
 
