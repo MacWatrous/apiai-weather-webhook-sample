d6a5e45cbbdfe69843e5f02b9f972819e07829b8
Watrous <mwatrous@ms.ds.uhc.com>
2016-07-07 16:11:02 -0400

    json fix

--- app.py
+++ app.py
@@ -96,8 +96,8 @@ def returnInquiry(req):
 
 	result = result.text
 	lhs, rhs = result.split("INDICATIONS AND USAGE ",1)
-	#lhs, rhs = rhs.split("\"]")
-	inquiry = rhs
+	lhs, rhs = rhs.split("tablets",1)
+	inquiry = lhs
 	return inquiry
 
 #def returnInteractions(rxcuiList):
