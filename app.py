69ba1d26961126538f46b5fe49d4768525c323aa
Watrous <mwatrous@ms.ds.uhc.com>
2016-07-08 11:50:12 -0400

    json fix

--- app.py
+++ app.py
@@ -137,8 +137,9 @@ def returnInteractions(req):
 
 	lhs, rhs = result.split("rxcui",1)
 	rhs = rhs[7:]
-	lhs, rhs = rhs.split("\"",1)
-	rxcui = lhs
+	rhs = rhs[:6]
+	#lhs, rhs = rhs.split("\"",1)
+	rxcui = rhs
 
 	#lhs, rhs = result2.split("rxcui \": [ \"",1)
 	#lhs, rhs = rhs.split("\"",1)
