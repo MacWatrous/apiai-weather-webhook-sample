4a39da449b7e070bf13f3fb5b4e3dc0be6fc8c09
Watrous <mwatrous@ms.ds.uhc.com>
2016-07-08 12:41:54 -0400

    json fix

--- app.py
+++ app.py
@@ -107,9 +107,9 @@ def returnInquiry(req):
 		print("help!")
 
 	result = result.text
-	lhs, rhs = result.split("INDICATIONS AND USAGE ",1)
-	lhs, rhs = rhs.split("\"",1)
-	inquiry = lhs
+	#lhs, rhs = result.split("INDICATIONS AND USAGE ",1)
+	#lhs, rhs = rhs.split("\"",1)
+	inquiry = result
 	return inquiry
 
 def returnInteractions(req):
