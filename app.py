85fb3dd64d7a644d6fdbad9c81e551f12df2c4d7
Watrous <mwatrous@ms.ds.uhc.com>
2016-07-08 13:04:15 -0400

    json fix

--- app.py
+++ app.py
@@ -56,6 +56,18 @@ def processRequest(req):
         	# "contextOut": [],
         	"source": "apiai-weather-webhook-sample"
     	}
+
+    if req.get("result").get("action") == "drugInteractionsPrior":
+    	#rxcui = returnRXCUI(req)
+    	inquiry = returnInteractionsPrior(req)
+    	speech = inquiry
+    	return {
+    		"speech": speech,
+       		"displayText": speech,
+        	# "data": data,
+        	# "contextOut": [],
+        	"source": "apiai-weather-webhook-sample"
+    	}
     	#ndc = returnNDC(rxcui)
 
     #else if req.get("result").get("action") == "drugInteractions":
@@ -118,6 +130,53 @@ def returnInteractions(req):
 	parameters = result.get("parameters")
 	drug = parameters.get("drug")
 	drug2 = parameters.get("drug1")
+
+	url = baseurl + "generic_name:\"" + drug + "\""
+	result = requests.get(url)
+	if result.status_code != 200:
+		url = baseurl + "brand_name:\"" + drug + "\""
+		result = (requests.get(url))
+		print("help!")
+	result = result.text
+
+	url2 = baseurl + "generic_name:\"" + drug2 + "\""
+	result2 = requests.get(url2)
+	if result2.status_code != 200:
+		url2 = baseurl + "brand_name:\"" + drug2 + "\""
+		result2 = (requests.get(url2))
+		print("help!2")
+	result2 = result2.text
+
+	lhs, rhs = result.split("rxcui",1)
+	rhs = rhs[16:]
+	rhs = rhs[:6]
+	rxcui = rhs
+
+	lhs, rhs = result2.split("rxcui",1)
+	rhs = rhs[16:]
+	rhs = rhs[:6]
+	rxcui2 = rhs
+
+	baseurl2 = "https://rxnav.nlm.nih.gov/REST/interaction/list.json?rxcuis="
+ 	url3 = baseurl2 + rxcui + "+" + rxcui2
+ 	result3 = requests.get(url3)
+	result3 = result3.text
+
+	if "description" in result3:
+		lhs, rhs = result3.split("description\":\"",1)
+		lhs, rhs = rhs.split("\"",1)
+		interaction = lhs
+		return interaction
+	return {}
+
+def returnInteractionsPrior(req)
+	baseurl = "https://api.fda.gov/drug/label.json?search=openfda."
+	result = req.get("result")
+	parameters = result.get("parameters")
+	drug2 = parameters.get("drug1")
+	contexts = result.get("contexts")
+	conParam = contexts.get("parameters")
+	drug = conParam.get("inquiredDrug")
 
 	url = baseurl + "generic_name:\"" + drug + "\""
 	result = requests.get(url)
