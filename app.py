28e69f8bc98d409236c1008cc50d0f83b94587b8
Watrous <mwatrous@ms.ds.uhc.com>
2016-07-08 14:49:33 -0400

    json fix

--- app.py
+++ app.py
@@ -173,10 +173,8 @@ def returnInteractionsPrior(req):
 	baseurl = "https://api.fda.gov/drug/label.json?search=openfda."
 	result = req.get("result")
 	parameters = result.get("parameters")
-	drug2 = parameters.get("drug")
-	contexts = result.get("contexts")
-	conParam = contexts.get("parameters")
-	drug = conParam.get("drug")
+	drug2 = parameters.get("drug1")
+	drug = parameters.get("drug")
 
 	url = baseurl + "generic_name:\"" + drug + "\""
 	result = requests.get(url)
