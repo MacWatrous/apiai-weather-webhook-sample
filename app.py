48517297f01b3613eb0257c3dc906acae5c7a5ac
Watrous <mwatrous@ms.ds.uhc.com>
2016-07-12 11:51:32 -0400

    json fix

--- app.py
+++ app.py
@@ -165,7 +165,7 @@ def returnInteractions(req):
 	result = result.text
 	lhs, rhs = result.split("rxcui",1)
 	rhs = rhs[16:]
-	lhs, rhs = rhs.split("\"",1)
+	lhs, rhs = rhs.split("\\\"",1)
 	rxcui = lhs
 
 	# if "true" == parameters.get("alcohol", "true"):
