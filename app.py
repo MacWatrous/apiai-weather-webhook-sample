1e8874c307d32c64987fbd60581c80c121b4fc3d
Watrous <mwatrous@ms.ds.uhc.com>
2016-07-12 09:36:48 -0400

    json fix

--- app.py
+++ app.py
@@ -122,6 +122,7 @@ def returnInquiry(req):
 		print("help!")
 		result = result.text
 		lhs, rhs = result.split("INDICATIONS AND USAGE ",1)
+		rhs.replace('\"',".")
 		lhs, rhs = rhs.split(".",1)
 		inquiry = lhs
 		return inquiry
