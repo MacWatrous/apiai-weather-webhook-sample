3c6f49db88cd6f9b8e82e544a54dada73343a404
Watrous <mwatrous@ms.ds.uhc.com>
2016-07-18 16:03:56 -0400

    delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-setup(name='app',
-      version='1.0',
-      description='MedBot',
-      author='Mac Watrous',
-      author_email='Macarthur.watrous@optum.com',
-      url='https://github.com/MacWatrous/apiai-weather-webhook-sample',
-     install_requires=['Flask==0.10.1', 'request==2.10.0'],
-     )
