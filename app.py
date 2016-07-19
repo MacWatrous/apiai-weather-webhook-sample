6dc9c9387266e6e2a2a82b0ffd053e45484999a4
Watrous <mwatrous@ms.ds.uhc.com>
2016-07-19 09:32:28 -0400

    fixed spacing

--- app.py
+++ app.py
@@ -10,104 +10,104 @@ from flask import Flask
 from flask import request
 from flask import make_response
 
-# Flask app should start in global layout
+#Flask app should start in global layout
 app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    req = request.get_json(silent=True, force=True)
+	req = request.get_json(silent=True, force=True)
 
-    print("Request:")
-    print(json.dumps(req, indent=4))
+	print("Request:")
+	print(json.dumps(req, indent=4))
 
-    res = processRequest(req)
+	res = processRequest(req)
 
-    res = json.dumps(res, indent=4)
-    # print(res)
-    r = make_response(res)
-    r.headers['Content-Type'] = 'application/json'
-    return r
+	res = json.dumps(res, indent=4)
+	#print(res)
+	r = make_response(res)
+	r.headers['Content-Type'] = 'application/json'
+	return r
 
 
 def processRequest(req):
-    #if req.get("result").get("action") != "yahooWeatherForecast":
-        #return {}
+	#if req.get("result").get("action") != "yahooWeatherForecast":
+		#return {}
 
-    if req.get("result").get("action") == "drugInquiry":
-    	inquiry = returnInquiry(req)
-    	speech = inquiry
-    	return {
-    		"speech": speech,
-       		"displayText": speech,
-        	# "data": data,
-        	# "contextOut": [],
-        	"source": "apiai-weather-webhook-sample"
-    	}
+	if req.get("result").get("action") == "drugInquiry":
+		inquiry = returnInquiry(req)
+		speech = inquiry
+		return {
+			"speech": speech,
+			"displayText": speech,
+			#"data": data,
+			#"contextOut": [],
+			"source": "apiai-weather-webhook-sample"
+		}
 
-    if req.get("result").get("action") == "drugInteractions":
-    	inquiry = returnInteractions(req)
-    	speech = inquiry
-    	return {
-    		"speech": speech,
-       		"displayText": speech,
-        	# "data": data,
-        	# "contextOut": [],
-        	"source": "apiai-weather-webhook-sample"
-    	}
+	if req.get("result").get("action") == "drugInteractions":
+		inquiry = returnInteractions(req)
+		speech = inquiry
+		return {
+			"speech": speech,
+			"displayText": speech,
+			#"data": data,
+			#"contextOut": [],
+			"source": "apiai-weather-webhook-sample"
+		}
 
-    if req.get("result").get("action") == "drugInteractionsPrior":
-    	inquiry = returnInteractionsPrior(req)
-    	speech = inquiry
-    	return {
-    		"speech": speech,
-       		"displayText": speech,
-        	# "data": data,
-        	# "contextOut": [],
-        	"source": "apiai-weather-webhook-sample"
-    	}
-    if req.get("result").get("action") == "drugRoute":
-    	inquiry = returnRoute(req)
-    	speech = inquiry
-    	return {
-    		"speech": speech,
-       		"displayText": speech,
-        	# "data": data,
-        	# "contextOut": [],
-        	"source": "apiai-weather-webhook-sample"
-    	}
-    else: 
-    	return {}
+	if req.get("result").get("action") == "drugInteractionsPrior":
+		inquiry = returnInteractionsPrior(req)
+		speech = inquiry
+		return {
+			"speech": speech,
+			"displayText": speech,
+			#"data": data,
+			#"contextOut": [],
+			"source": "apiai-weather-webhook-sample"
+		}
+	if req.get("result").get("action") == "drugRoute":
+		inquiry = returnRoute(req)
+		speech = inquiry
+		return {
+			"speech": speech,
+			"displayText": speech,
+			#"data": data,
+			#"contextOut": [],
+			"source": "apiai-weather-webhook-sample"
+		}
+	else: 
+		return {}
 
-    #yahoo stuff
-    #baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    #yql_query = makeYqlQuery(req)
-    #if yql_query is None:
-    #    return {}
-    #yql_url = baseurl + urllib.urlencode({'q': yql_query}) + "&format=json"
-    #result = urllib.urlopen(yql_url).read()
-    #data = json.loads(result)
-    #res = makeWebhookResult(data)
-    #return res
+	#yahoo stuff
+	#baseurl = "https://query.yahooapis.com/v1/public/yql?"
+	#yql_query = makeYqlQuery(req)
+	#if yql_query is None:
+	#    return {}
+	#yql_url = baseurl + urllib.urlencode({'q': yql_query}) + "&format=json"
+	#result = urllib.urlopen(yql_url).read()
+	#data = json.loads(result)
+	#res = makeWebhookResult(data)
+	#return res
 
-# def returnNDC(rxcui):
-# 	url = "https://rxnav.nlm.nih.gov/REST/ndcproperties.json?id=" + rxcui
-# 	result = (requests.get(url)).text
-# 	lhs, rhs = result.split("ndc9\": \"",1)
-# 	lhs, rhs = rhs.split("\",\"ndc10",1)
-# 	ndc = lhs
-# 	return ndc;
+#def returnNDC(rxcui):
+	#url = "https://rxnav.nlm.nih.gov/REST/ndcproperties.json?id=" + rxcui
+	#result = (requests.get(url)).text
+	#lhs, rhs = result.split("ndc9\": \"",1)
+	#lhs, rhs = rhs.split("\",\"ndc10",1)
+	#ndc = lhs
+	#return ndc;
 
-# def returnRXCUI(req):
-# 	baseurl = "https://rxnav.nlm.nih.gov/REST/approximateTerm.json?"
-# 	result = req.get("result")
-# 	parameters = result.get("parameters")
-# 	drug = parameters.get("drug")
-# 	url = baseurl + "term=" + drug + "&maxEntries=1"
-# 	result = (requests.get(url)).text
-# 	lhs, rhs = result.split("rxcui\":\"",1)
-# 	lhs, rhs = rhs.split("\",\"rxaui",1)
-# 	rxcui = lhs
-# 	return rxcui;
+#def returnRXCUI(req):
+	#baseurl = "https://rxnav.nlm.nih.gov/REST/approximateTerm.json?"
+	#result = req.get("result")
+	#parameters = result.get("parameters")
+	#drug = parameters.get("drug")
+	#url = baseurl + "term=" + drug + "&maxEntries=1"
+	#result = (requests.get(url)).text
+	#lhs, rhs = result.split("rxcui\":\"",1)
+	#lhs, rhs = rhs.split("\",\"rxaui",1)
+	#rxcui = lhs
+	#return rxcui;
 
 def returnInquiry(req):
 	baseurl = "https://api.fda.gov/drug/label.json?search=openfda."
@@ -186,8 +186,8 @@ def returnInteractions(req):
 		print("there's alcohol here")
 
 	baseurl2 = "https://rxnav.nlm.nih.gov/REST/interaction/list.json?rxcuis="
- 	url3 = baseurl2 + rxcui + "+" + rxcui2
- 	result3 = requests.get(url3)
+	url3 = baseurl2 + rxcui + "+" + rxcui2
+	result3 = requests.get(url3)
 	result3 = result3.text
 
 	if "severity" in result3:
@@ -233,8 +233,8 @@ def returnInteractionsPrior(req):
 		rxcui2 = "448"
 
 	baseurl2 = "https://rxnav.nlm.nih.gov/REST/interaction/list.json?rxcuis="
- 	url3 = baseurl2 + rxcui + "+" + rxcui2
- 	result3 = requests.get(url3)
+	url3 = baseurl2 + rxcui + "+" + rxcui2
+	result3 = requests.get(url3)
 	result3 = result3.text
 
 	if "severity" in result3:
@@ -245,57 +245,57 @@ def returnInteractionsPrior(req):
 	return "Looks like there is no interaction between these drugs."
 
 def makeYqlQuery(req):
-    result = req.get("result")
-    parameters = result.get("parameters")
-    city = parameters.get("geo-city")
-    if city is None:
-        return None
+	result = req.get("result")
+	parameters = result.get("parameters")
+	city = parameters.get("geo-city")
+	if city is None:
+		return None
 
-    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')" 
+	return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')" 
 
 def makeWebhookResult(data):
-    query = data.get('query')
-    if query is None:
-        return {}
+	query = data.get('query')
+	if query is None:
+		return {}
 
-    result = query.get('results')
-    if result is None:
-        return {}
+	result = query.get('results')
+	if result is None:
+		return {}
 
-    channel = result.get('channel')
-    if channel is None:
-        return {}
+	channel = result.get('channel')
+	if channel is None:
+		return {}
 
-    item = channel.get('item')
-    location = channel.get('location')
-    units = channel.get('units')
-    if (location is None) or (item is None) or (units is None):
-        return {}
+	item = channel.get('item')
+	location = channel.get('location')
+	units = channel.get('units')
+	if (location is None) or (item is None) or (units is None):
+		return {}
 
-    condition = item.get('condition')
-    if condition is None:
-        return {}
+	condition = item.get('condition')
+	if condition is None:
+		return {}
 
-    # print(json.dumps(item, indent=4))
+	#print(json.dumps(item, indent=4))
 
-    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+	speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
+			 ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
 
-    print("Response:")
-    print(speech)
+	print("Response:")
+	print(speech)
 
-    return {
-        "speech": speech,
-        "displayText": speech,
-        # "data": data,
-        # "contextOut": [],
-        "source": "apiai-weather-webhook-sample"
-    }
+	return {
+		"speech": speech,
+		"displayText": speech,
+		#"data": data,
+		#"contextOut": [],
+		"source": "apiai-weather-webhook-sample"
+	}
 
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
+	port = int(os.getenv('PORT', 5000))
 
-    print "Starting app on port %d" % port
+	print "Starting app on port %d" % port
 
-    app.run(debug=False, port=port, host='0.0.0.0')
+	app.run(debug=False, port=port, host='0.0.0.0')
