c2325c0c9992906051536db790c040265b819e27
macwatrous <mwatrous@ms.ds.uhc.com>
2016-07-25 13:38:01 -0400

    parse out interactions

--- app.py
+++ app.py
@@ -195,6 +195,7 @@ def returnInteractions(req):
         lhs, rhs = rhs.split("\"",1)
         interaction = lhs
         print(result3v2.json)
+        result3v2 = result3v2.json
         resultDrug = result3v2['fullInteractionTypeGroup'][0]['fullInteractionType'][0]['interactionPair'][0]['interactionConcept'][0]['minConceptItem']['name']
         resultDrug2 = result3v2['fullInteractionTypeGroup'][0]['fullInteractionType'][0]['interactionPair'][0]['interactionConcept'][1]['minConceptItem']['name']
         print(resultDrug)
